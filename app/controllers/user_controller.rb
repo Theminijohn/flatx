0bd6ef6fcc906fecba99bddaef02e5b810a5e0e5
The Mini John <admin@phoenix-prime.gr>
2013-08-16 22:49:03 +0300

    Fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
 	before_filter :authenticate_user!, only: :show
 
   def show
-    @user = User.includes(:listings).find(params[:id])
-		@listings = @user.map(&:listings) || []
+    @user = User.find(params[:id])
+    @listings = @user.listings
   end
 end
