fa0c26f292a5000af9e04d515e6478ceee66d60a
The Mini John <admin@phoenix-prime.gr>
2013-08-17 19:19:48 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Flat::Application.routes.draw do
 
   resources :categories
+	root :to => "listings#index"
 
   get "user/show"
   resources :listings
 
-	devise_for :users, :controllers => { :registration => "registration", :omniauth_callbacks => "users/omniauth_callbacks" }
-	get 'users/:id' => 'user#show', as: :user
+	get "flatuipro_demo/index"
 
-  get "flatuipro_demo/index"
-	root :to => "listings#index"
+	devise_for :users, :controllers => { :registration => "registration",
+																			 :omniauth_callbacks => "users/omniauth_callbacks" },
+						 :path => '', :path_names => {:sign_in => 'login'}
+	get 'users/:id' => 'user#show', as: :user
 
 	resources :messages, only: [:index, :create, :new] do
 		member do
